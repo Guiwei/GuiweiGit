4d73da732adc09aa7681908623212e43d5df7bdd
Guiwei Zheng <2465501155@qq.com>
2015-08-23 17:54:33 +0800

    First good version

--- JavaMail/SendMail.sh
+++ JavaMail/SendMail.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-SERVER_HOME=/home/gssalert/RedHatMail/build
-CLASSPATH=.:$CLASSPATH:/home/gssalert/RedHatMail/build/classes
-SERVER_LIB_PATH=/home/gssalert/RedHatMail/WebContent/WEB-INF/lib
+PROJECT_PATH=/media/guiwei/Files/GuiweiGit/JavaMail
+CLASSPATH=.:$CLASSPATH:$PROJECT_PATH/build/classes
+SERVER_LIB_PATH=$PROJECT_PATH/WebContent/WEB-INF/lib
 
 #Base library
-for  lname in `ls -rt $SERVER_LIB_PATH/*.jar | awk -F\/ '{print \$NF}'`
+for lname in `ls -rt $SERVER_LIB_PATH/*.jar | awk -F\/ '{print \$NF}'`
 do
-        CLASSPATH=$CLASSPATH:$SERVER_LIB_PATH/$lname
+	CLASSPATH=$CLASSPATH:$SERVER_LIB_PATH/$lname
 done
 
 export CLASSPATH
-echo "____________CLASSPATH_______________"
-echo $CLASSPATH
-echo "____________CLASSPATH_______________"
-
-#run
-#cd $SERVER_HOME
 
 for ANA_STA_PID in `ps -e -o pid -o args | grep SendMail | grep java |awk '{print $1}'`
 do
-        echo "SendMail is running,please wait is done"
-        exit 0
+	echo "You've a unfinished SendMail."
+	exit 0
 done
 
-echo "Start SendMail ... ...."
-file=`date +'%Y%m%d'`
+today=`date +'%Y%m%d'`
+nohup $JAVA_HOME/bin/java com.z.mail.SendMail 1 > SendMail.$today.log 2>&1 &
 
-nohup $JAVA_HOME/bin/java com.forlink.http.SendMail  1>>$SERVER_HOME/logs/SendMail.$file.log 2>&1 &
-
-sleep 10
+sleep 2
 for ANA_STA_PID in `ps -e -o pid -o args | grep SendMail | grep java |awk '{print $1}'`
 do
-        echo "SendMail PID: "$ANA_STA_PID
+	echo "SendMail PID: "$ANA_STA_PID
 done
---
+++ JavaMail/src/com/z/mail/SendMail.java
@@ -0,0 +1,289 @@
+package com.z.mail;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
+
+import org.apache.log4j.Logger;
+
+public class SendMail {
+	static Logger logger = Logger.getLogger(SendMail.class);
+
+	private static Map<String, String> mapFromInfo = new HashMap<String, String>();
+	private String mail_title;
+	private String mail_content;
+	private File[] mail_annex;
+
+	private static List<String> listTo = new ArrayList<String>();
+	private static List<String> listCopy = new ArrayList<String>();
+
+	private MimeMessage mimeMessage;
+	private Session session;
+	private Properties props;
+
+	private void readIni() {
+		mapFromInfo.clear();
+		mapFromInfo = readToMap("mail_from.ini");
+		logger.info("mail_from.ini:\r\n" + mapFromInfo.toString());
+
+		listTo.clear();
+		listTo = readToList("mail_to.ini");
+		logger.info("mail_to.ini:\r\n" + listTo.toString());
+
+		listCopy.clear();
+		listCopy = readToList("mail_copy.ini");
+		logger.info("mail_copy.ini:\r\n" + listTo.toString());
+	}
+
+	private void readMailTxt() {
+		mail_title = "";
+		mail_content = "";
+
+		FileReader fr = null;
+		try {
+			fr = new FileReader("./mail.txt");
+		} catch (FileNotFoundException e) {
+		}
+		BufferedReader br = new BufferedReader(fr);
+		String s = null;
+		try {
+			s = br.readLine();
+			mail_title = s;
+			while (null != (s = br.readLine())) {
+				mail_content += s;
+			}
+		} catch (IOException e) {
+		}
+	}
+
+	private static Map<String, String> readToMap(String file) {
+		Map<String, String> map = new HashMap<String, String>();
+		FileReader fr = null;
+		try {
+			fr = new FileReader(file);
+		} catch (FileNotFoundException e) {
+		}
+		BufferedReader br = new BufferedReader(fr);
+		String s = null;
+		while (true) {
+			try {
+				s = br.readLine();
+			} catch (IOException e) {
+			}
+			if (s == null || s.trim().length() == 0)
+				break;
+			if (!s.contains(":"))
+				continue;
+			String key = s.substring(0, s.indexOf(":")).trim();
+			if (key == null)
+				continue;
+			s = s.substring(s.indexOf(":") + 1);
+			if (s == null)
+				continue;
+			if (s.contains("#")) {
+				s = s.substring(0, s.indexOf("#")).trim();
+			} else {
+				s = s.trim();
+			}
+			if (s == null || s.length() == 0)
+				continue;
+			map.put(key, s);
+		}
+		return map;
+	}
+
+	private static List<String> readToList(String file) {
+		List<String> list = new ArrayList<String>();
+		FileReader fr = null;
+		try {
+			fr = new FileReader(file);
+		} catch (FileNotFoundException e) {
+		}
+		BufferedReader br = new BufferedReader(fr);
+		String s = null;
+		while (true) {
+			try {
+				s = br.readLine();
+			} catch (IOException e) {
+			}
+			if (s == null || s.trim().length() == 0)
+				break;
+			if (s.contains("#")) {
+				s = s.substring(0, s.indexOf("#")).trim();
+			} else {
+				s = s.trim();
+			}
+			if (s.equals("")) {
+				continue;
+			}
+			list.add(s);
+		}
+		return list;
+	}
+
+	private static String[] listToArray(List<String> list) {
+		String string = list.toString();
+		string = string.replace("[", "").replace("]", "");
+		String[] array = string.split(",");
+		if (array[0].trim().length() == 0) {
+			return null;
+		}
+		return array;
+	}
+
+	private void sendMail() throws Exception {
+		String user = mapFromInfo.get("User");
+		String password = mapFromInfo.get("Password");
+		String[] to = listToArray(listTo);
+		String[] copy = listToArray(listCopy);
+
+		try {
+			System.out.println("Start to send mail...");
+			Session mailSession = Session.getInstance(props, null);
+			Transport transport = mailSession.getTransport("smtp");
+			transport.connect((String) props.get("mail.smtp.host"), user, password);
+
+			String[] receive = mergeArray(to, copy);
+			Address[] adressreceive = new Address[receive.length];
+			for (int i = 0; i < adressreceive.length; ++i) {
+				adressreceive[i] = InternetAddress.parse(receive[i])[0];
+			}
+			transport.sendMessage(mimeMessage, adressreceive);
+			transport.close();
+			System.out.println("Mail sent!");
+
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	private static <T> T[] mergeArray(T[] array1, T[] array2) {
+		List<T> list = new ArrayList<T>();
+		if (array1 != null) {
+			for (T t : array1) {
+				list.add(t);
+			}
+		}
+		if (array2 != null) {
+			for (T t : array2) {
+				list.add(t);
+			}
+		}
+		return list.toArray(array1);
+	}
+
+	private void setMimeMessage() throws Exception {
+		String address = mapFromInfo.get("Address");
+		String user = mapFromInfo.get("User");
+		String password = mapFromInfo.get("Password");
+		String[] to = listToArray(listTo);
+		String[] copy = listToArray(listCopy);
+		mail_annex = null;
+
+		if (address == null || address == "") {
+			throw new Exception("Address is null");
+		}
+		if (user == null) {
+			throw new Exception("User is null");
+		}
+		if (password == null) {
+			throw new Exception("Password is null");
+		}
+		if (to == null) {
+			throw new Exception("To is null");
+		}
+		if (mail_title == null) {
+			mail_title = "";
+		}
+		if (mail_content == null) {
+			mail_content = "";
+		}
+
+		// set Properties
+		props = System.getProperties();
+		props.put("mail.smtp.host", mapFromInfo.get("SMTP_Host"));
+		props.put("mail.smtp.port", Integer.parseInt(mapFromInfo.get("SMTP_Port")));
+		if (mapFromInfo.get("Auth").equalsIgnoreCase("true")) {
+			props.put("mail.smtp.auth", "true");
+		} else {
+			props.put("mail.smtp.auth", "false");
+		}
+
+		session = Session.getDefaultInstance(props, null);
+		mimeMessage = new MimeMessage(session);
+		mimeMessage.setFrom(new InternetAddress(address));
+		mimeMessage.setSender(new InternetAddress(address));
+		mimeMessage.setSubject(mail_title);
+		mimeMessage.setHeader("Content-Transfer-Encoding", "7bit");
+
+		// set To
+		Address[] adress = new Address[to.length];
+		for (int i = 0; i < adress.length; ++i) {
+			adress[i] = InternetAddress.parse(to[i])[0];
+		}
+		mimeMessage.setRecipients(Message.RecipientType.TO, adress);
+
+		// set CC
+		if (copy != null && copy.length != 0) {
+			Address[] replyto = new Address[copy.length];
+			for (int i = 0; i < copy.length; ++i) {
+				replyto[i] = InternetAddress.parse(copy[i])[0];
+			}
+			mimeMessage.addRecipients(Message.RecipientType.CC, replyto);
+		}
+		mimeMessage.setDescription("description");
+
+		// Annex or just text
+		if (mail_annex == null || mail_annex.length == 0) {
+			mimeMessage.setContent(mail_content, "text/html;charset=gbk");
+		} else {
+			Multipart mp = new MimeMultipart();
+			MimeBodyPart mbpC = new MimeBodyPart();
+			mbpC.setContent(mail_content, "text/html;charset=gbk");
+			mp.addBodyPart(mbpC);
+			for (File f : mail_annex) {
+				MimeBodyPart mbp = new MimeBodyPart();
+				mbp.setFileName(MimeUtility.encodeText(f.getName()));
+				FileDataSource fds = new FileDataSource(f);
+				mbp.setDataHandler(new DataHandler(fds));
+				mp.addBodyPart(mbp);
+			}
+			mimeMessage.setContent(mp);
+		}
+
+		mimeMessage.saveChanges();
+	}
+
+	public static void main(String[] args) {
+		try {
+			SendMail sm = new SendMail();
+			sm.readIni();
+			sm.readMailTxt();
+			sm.setMimeMessage();
+			sm.sendMail();
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+
+}
